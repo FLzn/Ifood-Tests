b5472b276f5d4e977d83f85d35a008fa7552858f
Rafael Gonçalves <rafagpoliveira@gmail.com>
2021-06-21 09:19:36 -0300

    Alteração mínima

--- controllers/lojas.js
+++ controllers/lojas.js
@@ -7,7 +7,7 @@ module.exports = app => {
         res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
         next();
-        });
+    });
 
     app.get('/lojas', (req,res) => {
         Loja.read(res)
